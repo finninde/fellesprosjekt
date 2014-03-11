3634288d0dbc74f546d68fcc71fa87b2fe47d782
carnessir <andrewie@stud.ntnu.no>
2014-03-11 11:59:00 +0100

    SendMail ready to be filled in

--- src/main/java/helperclasses/SendMail.java
+++ src/main/java/helperclasses/SendMail.java
@@ -17,14 +17,14 @@ package helperclasses;
 
 public class SendMail {
 
-    public SendMail(String farmer_mail, String farmer_name, String timestamp, String sheepId, String sheepX, String sheepY) {
+    public SendMail(/*FILL IN DESIRED PARAMETERS*/String inviter, Appointment appointment, String invitedEmail) {
 
         /**
          * Set up the mail sender.
          */
 
-        final String username = "g21.sheepalert@gmail.com";			//mailadressen som sender mail
-        final String password = "imsdallomper";						//passord
+        final String username = ""/*FILL IN DESIRED SENDER MAIL*/;			//mailadressen som sender mail
+        final String password = ""/*FILL IN PASSWORD TO SENDER MAIL*/;						//passord
 
         Properties props = new Properties();
         props.put("mail.smtp.starttls.enable", "true");
@@ -47,11 +47,9 @@ public class SendMail {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(username));
             message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(farmer_mail));					//mail-addresse til mottaker
-            message.setSubject("ALARM! Your sheep is under attack!!!");
-            message.setText(farmer_name	+ ","								//innhold i mailen
-                    + "\n\n Your sheep, ID: " + sheepId + " is under attack by something, could be a wolf (or aliens)! \n Attack happened at "+timestamp+"!"
-                    + "\n\n https://maps.google.com/maps/api/staticmap?size=300x300&maptype=satellite&sensor=false&markers=color:red%7Clabel:S%7C"+sheepX+","+sheepY);
+                    InternetAddress.parse(invitedEmail));					//mail-addresse til mottaker
+            message.setSubject(inviter+" has invited you to his appointment");
+            message.setText(inviter	+ " wants you to participate in the appointment..."/*FILL IN REST OF MESSAGE*/);
 
             Transport.send(message);
 
