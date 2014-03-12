cb72b5252207afe3fd60086378e2418e4c081f71
Kristoffer Dalby <kradalby@kradalby.no>
2014-03-12 14:59:34 +0100

    added some missing fields

--- src/main/java/helperclasses/Alarm.java
+++ src/main/java/helperclasses/Alarm.java
@@ -7,10 +7,47 @@ import org.joda.time.DateTime;
  */
 public class Alarm {
 
+    private int id;
     private DateTime executeAlarm;
+    private User user;
+    private Appointment appointment;
+
+    public Alarm() {
+    }
 
     public boolean executeAlarm() throws NotYetImplementedException {
         throw new NotYetImplementedException();
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public DateTime getExecuteAlarm() {
+        return executeAlarm;
+    }
+
+    public void setExecuteAlarm(DateTime executeAlarm) {
+        this.executeAlarm = executeAlarm;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
 }
