6b7ea943116f671d8fa8926eb46d3f3ee544aca2
Kristoffer Dalby <kradalby@kradalby.no>
2014-03-19 16:10:17 +0100

    implemented alarm methods and update status for the db

--- src/main/java/database/AppointmentService.java
+++ src/main/java/database/AppointmentService.java
@@ -14,16 +14,20 @@ public interface AppointmentService {
     public void addTimeFrame(TimeFrame timeFrame);
     public void addParticipant(Participant participant, Appointment appointment);
     public void addParticipants(ArrayList<Participant> participants, Appointment appointment);
+    public void addAlarm(Alarm alarm);
 
     public Appointment getAppointment(int id);
     public TimeFrame getTimeFrame(int id);
     public ArrayList<Participant> getParticipants(int id);
+    public Alarm getAlarm(String username, int appointmentID);
 
     public String updateAppointment(Appointment appointment);
     public void updateTimeFrame(TimeFrame timeFrame);
+    public void updateAlarm(Alarm alarm);
 
     public void deleteParticipant(Participant participant, Appointment appointment);
     public void deleteAppointment(Appointment appointment);
+    public void deleteAlarm(Alarm alarm);
 
     public void updateParticipantStatus(int AppointmentID, Status newStatus, String username);
 
